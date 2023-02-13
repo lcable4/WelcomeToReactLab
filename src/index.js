861b703c3830e335cbb9af17d3e2f593428b27d8
Logan <logancable@ymail.com>
2023-02-13 10:53:17 -0800

    3rd commit, added components

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Counter = () => {
+    const greeting = "Hello"
     const [count, setCount] = useState(0);
     const Increment = () => {
         setCount(count + 1)
+    }
+    const Decrement = () => {
+        setCount(count - 1)
     }
     return (
       <div id='container'>
         <div id='navbar'>
           Counter.js
         </div>
+        {greeting}
         <div id='counter'>
           <h1>{count}</h1>
           <button onClick = {Increment}>Increment</button>
+          <button onClick = {Decrement}>Decrement</button>
         </div>
+
       </div>
     )
   }
